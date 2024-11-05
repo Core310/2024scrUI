0eb8cbe1e858b4100bf0a8f1a518fb95c2fdb1dc
Something Something <arika.khor@gmail.com>
2024-11-05 14:15:32 -0600

    have activeTabs working...

--- scripts/pageNavigator.js
+++ scripts/pageNavigator.js
@@ -1,16 +1,26 @@
 $(document).keydown(function (e) {
-    const activeLink = document.querySelector('a.nav-link.active');
-    let newActiveLink;
 
+    const activeLink = document.querySelector('a.nav-link.active');
+
+    let newActiveLink;
     switch (e.which) {
         case 37: // left
-            i === dataPageIds.length ? i = 0 : i++;
-            const activeNav = $('.nav-link.active');
-            activeNav.removeClass('active');
-            const oldPageId = activeNav.attr('data-page-id');
-            $('#' + oldPageId).hide();
-            const pageId = dataPageIds[i];
+            i === dataPageIds.length ? i = 0 : i++;//TODO 5/11/2024 Get the current navigation to take into account the current index of whatever tab ur currently on
+
+
+            const activeNav = $('.nav-link.active'); //grabs all elements with .nav-link.active classes
+            activeNav.removeClass('active');//Disable current tab
+            const oldPageId = activeNav.attr('data-page-id'); //Current page id
+            $('#' + oldPageId).hide(); //hide old page with IDD
+
+            const pageId = $(`.nav-link[data-page-id="${dataPageIds[i]}"]`); //new page ID to be shown where (this) is the clicked element
+            pageId.addClass('active');//make clicked element active
+
             $('#' + pageId).show();
+
+
+            //new code above
+
             break;
         case 39: // right
             newActiveLink = activeLink.parentElement.nextElementSibling?.querySelector('a.nav-link.active');
--- scripts/tabs.js
+++ scripts/tabs.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
 
     // When any a tag with a class of nav-link is clicked, get the corresponding div of page with data-page-id attribute and show it
     $('.nav-link').on('click', function () {
-        const activeNav = $('.nav-link.active');
-        activeNav.removeClass('active');
-        const oldPageId = activeNav.attr('data-page-id');
-        $('#' + oldPageId).hide();
+        const activeNav = $('.nav-link.active'); //grabs all elements with .nav-link.active classes
+        activeNav.removeClass('active');//Disable current tab
+        const oldPageId = activeNav.attr('data-page-id'); //Current page id
+        $('#' + oldPageId).hide(); //hide old page with IDD
 
-        $(this).addClass('active');
+        $(this).addClass('active');//make clicked element active
 
-        const pageId = $(this).attr('data-page-id');
+        const pageId = $(this).attr('data-page-id'); //new page ID to be shown where (this) is the clicked element
         $('#' + pageId).show();
     });
 })
